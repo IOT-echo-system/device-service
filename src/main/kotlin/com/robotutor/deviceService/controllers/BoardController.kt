fd9393cd3de6205c02ae60f2746347e5ddbfb49e
Shivam Rajput <raj.shiviraj@gmail.com>
2025-01-08 13:37:24 +0530

    Shivi | update board controller endpoints.

--- src/main/kotlin/com/robotutor/deviceService/controllers/BoardController.kt
+++ src/main/kotlin/com/robotutor/deviceService/controllers/BoardController.kt
@@ -2,9 +2,9 @@ package com.robotutor.deviceService.controllers
 
 import com.robotutor.deviceService.controllers.view.BoardRequest
 import com.robotutor.deviceService.controllers.view.BoardView
-import com.robotutor.deviceService.models.PremisesId
 import com.robotutor.deviceService.services.BoardService
 import com.robotutor.iot.utils.filters.annotations.RequirePolicy
+import com.robotutor.iot.utils.models.PremisesData
 import com.robotutor.iot.utils.models.UserData
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
@@ -12,22 +12,22 @@ import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @RestController
-@RequestMapping("/premises/{premisesId}/boards")
+@RequestMapping("/premises/*/boards")
 class BoardController(private val boardService: BoardService) {
 
     @RequirePolicy("DEVICE:CREATE")
     @PostMapping
     fun createBoard(
-        @PathVariable premisesId: PremisesId,
         @RequestBody @Validated boardRequest: BoardRequest,
         userData: UserData,
+        premisesData: PremisesData
     ): Mono<BoardView> {
-        return boardService.addBoard(premisesId, boardRequest, userData).map { BoardView.from(it) }
+        return boardService.addBoard(boardRequest, userData, premisesData).map { BoardView.from(it) }
     }
 
     @RequirePolicy("DEVICE:READ")
     @GetMapping
-    fun getBoards(@PathVariable premisesId: PremisesId, userData: UserData): Flux<BoardView> {
-        return boardService.getBoards(premisesId, userData).map { BoardView.from(it) }
+    fun getBoards(premisesData: PremisesData): Flux<BoardView> {
+        return boardService.getBoards(premisesData).map { BoardView.from(it) }
     }
 }
--- src/main/kotlin/com/robotutor/deviceService/services/BoardService.kt
+++ src/main/kotlin/com/robotutor/deviceService/services/BoardService.kt
@@ -1,14 +1,17 @@
 package com.robotutor.deviceService.services
 
 import com.robotutor.deviceService.controllers.view.BoardRequest
-import com.robotutor.deviceService.gateway.PremisesGateway
 import com.robotutor.deviceService.models.Board
 import com.robotutor.deviceService.models.IdType
-import com.robotutor.deviceService.models.PremisesId
 import com.robotutor.deviceService.repositories.BoardRepository
 import com.robotutor.iot.auditOnError
 import com.robotutor.iot.auditOnSuccess
+import com.robotutor.iot.exceptions.UnAuthorizedException
 import com.robotutor.iot.service.IdGeneratorService
+import com.robotutor.iot.utils.createMonoError
+import com.robotutor.iot.utils.exceptions.IOTError
+import com.robotutor.iot.utils.gateway.views.PremisesRole
+import com.robotutor.iot.utils.models.PremisesData
 import com.robotutor.iot.utils.models.UserData
 import com.robotutor.iot.utils.utils.toMap
 import com.robotutor.loggingstarter.logOnError
@@ -21,15 +24,19 @@ import reactor.core.publisher.Mono
 class BoardService(
     private val boardRepository: BoardRepository,
     private val idGeneratorService: IdGeneratorService,
-    private val premisesGateway: PremisesGateway
 ) {
-    fun addBoard(premisesId: PremisesId, boardRequest: BoardRequest, userData: UserData): Mono<Board> {
+    fun addBoard(boardRequest: BoardRequest, userData: UserData, premisesData: PremisesData): Mono<Board> {
         val boardRequestMap = boardRequest.toMap().toMutableMap()
-        return premisesGateway.getPremisesByOwner(premisesId)
-            .flatMap { idGeneratorService.generateId(IdType.BOARD_ID) }
+        if (premisesData.user.role != PremisesRole.OWNER) {
+            return createMonoError<Board>(UnAuthorizedException(IOTError.IOT0104))
+                .auditOnError("DEVICE_CREATE", boardRequestMap)
+                .logOnError("", "Failed to create board!", additionalDetails = boardRequestMap)
+        }
+
+        return idGeneratorService.generateId(IdType.BOARD_ID)
             .flatMap { boardId ->
                 boardRequestMap["boardId"] = boardId
-                val board = Board.from(boardId, premisesId, boardRequest, userData)
+                val board = Board.from(boardId, premisesData.premisesId, boardRequest, userData)
                 boardRepository.save(board)
                     .auditOnSuccess("DEVICE_CREATE", boardRequestMap)
             }
@@ -39,8 +46,8 @@ class BoardService(
 
     }
 
-    fun getBoards(premisesId: PremisesId, userData: UserData): Flux<Board> {
-        return boardRepository.findAllByPremisesId(premisesId)
+    fun getBoards(premisesId: PremisesData): Flux<Board> {
+        return boardRepository.findAllByPremisesId(premisesId.premisesId)
     }
 }
 
